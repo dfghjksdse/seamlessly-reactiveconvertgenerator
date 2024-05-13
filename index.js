24a8957d0e56934f791c8e207705949d518c5ea5
dfghjksdse <rsjcafaelbrotto@hotmail.com>
2024-05-13 09:53:35 +0700

    add more features

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = new Array(n + 1).fill(0);
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
+function exist(board, word) {
+  const rows = board.length;
+  const cols = board[0].length;
+  const dfs = (i, j, index) => {
+    if (index === word.length) return true;
+    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] !== word[index])
+      return false;
+    const temp = board[i][j];
+    board[i][j] = "";
+    const found =
+      dfs(i + 1, j, index + 1) ||
+      dfs(i - 1, j, index + 1) ||
+      dfs(i, j + 1, index + 1) ||
+      dfs(i, j - 1, index + 1);
+    board[i][j] = temp;
+    return found;
+  };
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (board[i][j] === word[0] && dfs(i, j, 0)) return true;
     }
   }
-  return dp[0];
+  return false;
 }
